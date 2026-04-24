8ee5f23e10915411eac29d543689d7e82643ff6a
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-04-24 08:48:07 +0000

    update: correct path of files in test run

--- commands/deblur_filter.sh
+++ commands/deblur_filter.sh
@@ -1,4 +1,4 @@
 python -m pdb -m q2cli quality-filter q-score \
-  --i-demux demux.qza \
-  --o-filtered-sequences demux-filtered.qza \
-  --o-filter-stats demux-filter-stats.qza
+  --i-demux /workspaces/qiime2_blog/test_data/demux.qza \
+  --o-filtered-sequences /workspaces/qiime2_blog/test_data/demux-filtered.qza \
+  --o-filter-stats /workspaces/qiime2_blog/test_data/demux-filter-stats.qza
--- commands/deblur_run.sh
+++ commands/deblur_run.sh
@@ -1,7 +1,7 @@
 python -m pdb -m q2cli denoise-16S \
-  --i-demultiplexed-seqs demux-filtered.qza \
+  --i-demultiplexed-seqs /workspaces/qiime2_blog/test_data/demux-filtered.qza \
   --p-trim-length 120 \
   --p-sample-stats \
-  --o-representative-sequences rep-seqs-deblur.qza \
-  --o-table table-deblur.qza \
-  --o-stats deblur-stats.qza
+  --o-representative-sequences /workspaces/qiime2_blog/test_data/rep-seqs-deblur.qza \
+  --o-table /workspaces/qiime2_blog/test_data/table-deblur.qza \
+  --o-stats /workspaces/qiime2_blog/test_data/deblur-stats.qza
