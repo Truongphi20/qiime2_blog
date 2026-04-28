9e66a2c85e842bfce8830a152afdd9239f78830b
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-04-28 09:57:23 +0000

    update: track down c api commands in mafft

--- commands/deblur_mafft.sh
+++ commands/deblur_mafft.sh
@@ -10,10 +10,25 @@ bashdb /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft --quiet \
 ## Real command: 
 ## because gdb dont be compatible with bash operator "<", run executor fist, then add args to run
 
+# /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2307
+# /opt/conda/envs/qiime2-amplicon-2026.1/libexec/mafft/replaceu -i orig > infile 2>>/dev/null
+
 # /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2673
 # gdb /workspaces/qiime2_blog/commands/mafft-7.525-with-extensions/build/core/disttbfast
-## ADD to run: (gdb) set args -q 0 -E 2 -V -1.53 -s 0.0 -W 6 -O -C 1-1 -b 62 -g 0 -f -1.53 -Q 100.0 -h 0 -F -X 0.1 -x 100 < infile
+# (gdb) set args -q 0 -E 2 -V -1.53 -s 0.0 -W 6 -O -C 1-1 -b 62 -g 0 -f -1.53 -Q 100.0 -h 0 -F -X 0.1 -x 100 < infile
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2691
+# gdb /opt/conda/envs/qiime2-amplicon-2026.1/libexec/mafft/dndpre
+# (gdb) set args -y hat2 -b 62 -M 2 -C 1 < pre     > /dev/null 2>>/dev/null
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2693
+# gdb /opt/conda/envs/qiime2-amplicon-2026.1/libexec/mafft/dvtditr
+# (gdb) set args -W 0.00001 -E 0.0 -s 0.0 -C 1 -t 0 -F -z 50 -b 62 -f -1.53 -Q 100.0 -h 0 -I 2 -X 0.1 -p BAATARI2 -K 0 < pre     > /dev/null 2>>/dev/null
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2699
+# gdb /opt/conda/envs/qiime2-amplicon-2026.1/libexec/mafft/restoreu
+# (gdb) set args -a pre -i orig > restored
 
 # /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2804
 # gdb /workspaces/qiime2_blog/commands/mafft-7.525-with-extensions/build/core/f2cl
-## ADD to run: (gdb) set args -n -1 -f -l 60 < /workspaces/qiime2_blog/support_data/mafft_tmp/pre
+# (gdb) set args -n -1 -f -l 60 < /workspaces/qiime2_blog/support_data/mafft_tmp/pre
