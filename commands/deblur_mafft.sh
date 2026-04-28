49b05d94c4f216ae4eb895136aa1e364bb540cd3
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-04-28 04:50:10 +0000

    update: add real debug command of f2cl

--- commands/deblur_mafft.sh
+++ commands/deblur_mafft.sh
@@ -7,7 +7,7 @@
 #         --thread 1 \
 #         /workspaces/qiime2_blog/support_data/L6S68_30_L001_R1_001.fastq.gz.trim.derep.no_artifacts
 
-## Real command
+## Real command: /opt/conda/envs/qiime2-amplicon-2026.1/bin/mafft:2804
 ## because gdb dont be compatible with bash operator "<", run executor fist, then add args to run 
 gdb /workspaces/qiime2_blog/commands/mafft-7.525-with-extensions/build/core/f2cl
 
