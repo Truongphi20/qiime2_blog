1139ae923056ba91a16e47ac174321a4bb27deb9
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-04-28 03:30:29 +0000

    update: draft add mafft source code and command

---
+++ commands/deblur_mafft.sh
@@ -0,0 +1,6 @@
+mafft --quiet \
+        --preservecase \
+        --parttree \
+        --auto \
+        --thread 1 \
+        /workspaces/qiime2_blog/support_data/L6S68_30_L001_R1_001.fastq.gz.trim.derep.no_artifacts
