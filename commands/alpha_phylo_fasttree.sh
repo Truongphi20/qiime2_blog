3bb506c228991941eac8f8bdc1ab67a52cdb6999
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-02 13:18:32 +0000

    chores: add commit id in fasttree package

--- commands/alpha_phylo_fasttree.sh
+++ commands/alpha_phylo_fasttree.sh
@@ -1,1 +1,1 @@
-gdb --args /workspaces/qiime2_blog/commands/fasttree/build/FastTree -quote -nt /workspaces/qiime2_blog/support_data/aligned-dna-sequences.fasta
+gdb --args /workspaces/qiime2_blog/commands/fasttree_29c5e62/build/FastTree -quote -nt /workspaces/qiime2_blog/support_data/aligned-dna-sequences.fasta
