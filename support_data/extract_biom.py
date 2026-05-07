21859963998b71b72c3bf3f1a6c98af0fbedd952
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-07 10:17:18 +0000

    update: prepare test input

---
+++ support_data/extract_biom.py
@@ -0,0 +1,8 @@
+from biom import load_table
+
+# 1. Load the table from DADA2 biom table
+table = load_table('/workspaces/qiime2_blog/support_data/feature-table.biom')
+
+# 4. Subset first 10 observes and 10 samples
+with open('mini-feature-table.tsv', 'w') as f:
+    f.write(table.head(10,10).to_tsv())
