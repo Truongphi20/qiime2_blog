71a8e0b6db289624143532b0079523ef05c151ab
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-07 15:49:36 +0000

    update: dive in jaccard

--- algorithm_reference/jaccard.py
+++ algorithm_reference/jaccard.py
@@ -3,7 +3,75 @@ import pandas as pd
 import numpy as np
 import skbio
 import sklearn.metrics
+from sklearn.utils.validation import check_array
 from skbio.stats.distance import DistanceMatrix
+from scipy.spatial import distance
+from scipy.spatial import _distance_wrap
+from scipy.spatial.distance import _METRIC_ALIAS
+from scipy._lib._util import _asarray_validated
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/sklearn/metrics/pairwise.py:72
+def check_pairwise_arrays(
+    X,
+    Y,
+    *,
+    precomputed=False,
+    dtype=None,
+    accept_sparse="csr",
+    force_all_finite=True,
+    copy=False,
+):
+    estimator = "check_pairwise_arrays"
+    X = Y = check_array(
+            X,
+            accept_sparse=accept_sparse,
+            dtype=float,
+            copy=copy,
+            force_all_finite=force_all_finite,
+            estimator=estimator,
+        )
+    return X, Y
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/scipy/spatial/distance.py:1864
+def pdist(X, metric='euclidean', *, out=None, **kwargs):
+    X = _asarray_validated(X, sparse_ok=False, objects_ok=True, mask_ok=True,
+                           check_finite=False)
+    s = X.shape
+    m, n = s
+    mstr = metric.lower()
+    metric_info = _METRIC_ALIAS.get(mstr, None)
+    pdist_fn = metric_info.pdist_func
+    return pdist_fn(X, out=out, **kwargs)
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/scipy/spatial/distance.py:2196
+def squareform(X, force="no", checks=True):
+    X = np.ascontiguousarray(X)
+    s = X.shape
+
+    # Grab the closest value to the square root of the number
+    # of elements times 2 to see if the number of elements
+    # is indeed a binomial coefficient.
+    d = int(np.ceil(np.sqrt(s[0] * 2)))
+
+    # Allocate memory for the distance matrix.
+    M = np.zeros((d, d), dtype=X.dtype)
+
+    # Fill in the values of the distance matrix.
+    _distance_wrap.to_squareform_from_vector_wrap(M, X)
+
+    p# Return the distance matrix.
+    return M
+
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/sklearn/metrics/pairwise.py:2168
+def pairwise_distances(X, Y=None, metric="euclidean", *, n_jobs=None, force_all_finite=True, **kwds):
+    X, Y = check_pairwise_arrays(
+            X, Y, dtype=bool, force_all_finite=force_all_finite
+        )
+    # precompute data-derived metric params
+    params = {}
+    kwds.update(**params)
+    return distance.squareform(pdist(X, metric=metric, **kwds))
 
 # /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/skbio/diversity/_driver.py:367
 def beta_diversity(metric, counts, ids=None, pairwise_func=None):
@@ -19,7 +87,7 @@ def jaccard(table: biom.Table, n_jobs: int = 1) -> skbio.DistanceMatrix:
             metric='jaccard', 
             counts=counts, 
             ids=sample_ids,
-            pairwise_func=sklearn.metrics.pairwise_distances
+            pairwise_func=pairwise_distances
     )
     return jaccard_table.to_data_frame()
 
