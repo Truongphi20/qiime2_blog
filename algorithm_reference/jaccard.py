0b7a15dcc7aca5900e29852cb5e27537b0e2e9c6
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-08 04:17:50 +0000

    update: clean up jaccard

--- algorithm_reference/jaccard.py
+++ algorithm_reference/jaccard.py
@@ -10,28 +10,6 @@ from scipy.spatial import _distance_wrap
 from scipy.spatial.distance import _METRIC_ALIAS
 from scipy._lib._util import _asarray_validated
 
-# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/sklearn/metrics/pairwise.py:72
-def check_pairwise_arrays(
-    X,
-    Y,
-    *,
-    precomputed=False,
-    dtype=None,
-    accept_sparse="csr",
-    force_all_finite=True,
-    copy=False,
-):
-    estimator = "check_pairwise_arrays"
-    X = Y = check_array(
-            X,
-            accept_sparse=accept_sparse,
-            dtype=float,
-            copy=copy,
-            force_all_finite=force_all_finite,
-            estimator=estimator,
-        )
-    return X, Y
-
 # /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/scipy/spatial/distance.py:1864
 def pdist(X, metric='euclidean', *, out=None, **kwargs):
     X = _asarray_validated(X, sparse_ok=False, objects_ok=True, mask_ok=True,
@@ -59,19 +37,16 @@ def squareform(X, force="no", checks=True):
     # Fill in the values of the distance matrix.
     _distance_wrap.to_squareform_from_vector_wrap(M, X)
 
-    p# Return the distance matrix.
+    # Return the distance matrix.
     return M
 
 
 # /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/sklearn/metrics/pairwise.py:2168
 def pairwise_distances(X, Y=None, metric="euclidean", *, n_jobs=None, force_all_finite=True, **kwds):
-    X, Y = check_pairwise_arrays(
-            X, Y, dtype=bool, force_all_finite=force_all_finite
-        )
     # precompute data-derived metric params
     params = {}
     kwds.update(**params)
-    return distance.squareform(pdist(X, metric=metric, **kwds))
+    return squareform(pdist(X, metric=metric, **kwds))
 
 # /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/skbio/diversity/_driver.py:367
 def beta_diversity(metric, counts, ids=None, pairwise_func=None):
