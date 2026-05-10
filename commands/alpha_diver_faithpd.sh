cb817e90c2277a15b7b7904eff85ad4ea1b2f7a1
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-10 06:42:35 +0000

    update: extract command of faithpd

---
+++ commands/alpha_diver_faithpd.sh
@@ -0,0 +1,5 @@
+touch /workspaces/qiime2_blog/test_data/faithpd/q2-OutPath.txt
+
+faithpd -i /workspaces/qiime2_blog/support_data/q2-OutPath-zwprhuvh\
+        -t /workspaces/qiime2_blog/support_data/tree.nwk\
+        -o /workspaces/qiime2_blog/test_data/faithpd/q2-OutPath.txt
