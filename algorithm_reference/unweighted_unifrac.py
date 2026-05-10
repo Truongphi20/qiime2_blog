30061ba8b47def8f858830e9d036966d5975baed
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-10 13:58:39 +0000

    update: dive in unweight unifrac

--- algorithm_reference/unweighted_unifrac.py
+++ algorithm_reference/unweighted_unifrac.py
@@ -33,6 +33,26 @@ def squareform(X):
 
     return M
 
+# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/scipy/spatial/distance.py:2627
+def _pdist_callable(X, *, out, metric, **kwargs):
+    n = X.shape[0]
+    out_size = (n * (n - 1)) // 2
+    dm = np.empty((out_size,), dtype=np.float64)
+    k = 0
+    for i in range(X.shape[0] - 1):
+        for j in range(i + 1, X.shape[0]):
+            dm[k] = metric(X[i], X[j], **kwargs)
+            k += 1
+    return dm
+
+
+# /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/scipy/spatial/distance.py:1864
+def pdist(X, metric='euclidean', *, out=None, **kwargs):
+    s = X.shape
+    m, n = s
+
+    return _pdist_callable(X, metric=metric, out=out, **kwargs)
+
 # /opt/conda/envs/qiime2-amplicon-2026.1/lib/python3.10/site-packages/skbio/diversity/_driver.py:367
 def beta_diversity(
     metric, counts, ids=None, validate=True, pairwise_func=None, **kwargs
@@ -43,7 +63,7 @@ def beta_diversity(
         )
     counts = counts_by_node
     
-    distances = scipy.spatial.distance.pdist(counts, metric=metric, **kwargs)
+    distances = pdist(counts, metric=metric, **kwargs)
     data = squareform(distances)
 
     return pd.DataFrame(data, columns=ids, index=ids)
